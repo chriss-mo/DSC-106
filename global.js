bd1bdd949efcd528761bc19525edc86ca054285e
csmo1112 <mo.chris1112@gmail.com>
2025-01-22 13:59:21 -0800

    remove current from classes, update global.js

--- global.js
+++ global.js
@@ -3,3 +3,48 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// const navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// currentLink.classList.add('current');
+
+// currentLink?.classList.add('current');
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'cv/', title: 'CV' },
+  { url: 'contact/', title: 'Contact' },
+  // add the rest of your pages here
+];
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  
+  // if (a.host === location.host && a.pathname === location.pathname) {
+  //   a.classList.add('current');
+  // }
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  nav.append(a);
+}
+
