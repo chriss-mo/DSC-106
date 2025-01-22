7c8fbe8b2727e3104336f8907d46fc4b4ad04117
csmo1112 <mo.chris1112@gmail.com>
2025-01-22 14:28:33 -0800

    update style and global

--- global.js
+++ global.js
@@ -37,10 +37,15 @@ for (let p of pages) {
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
-  
+
   if (a.host === location.host && a.pathname === location.pathname) {
     a.classList.add('current');
   }
+
+  if (target) {
+    a.target = target;
+  }
+  
   a.classList.toggle(
     'current',
     a.host === location.host && a.pathname === location.pathname
